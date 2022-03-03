94854ec27701a559198bbb12a86c58ac80565b2e
Steven <stevenescobar131@gmail.com>
2022-03-02 19:41:05 -0600

    Working on some DOM exercises

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log("Hellorino");
+
+const whole = document.querySelector('body');
+const para = document.createElement('p');
+para.textContent = "Hey I'm red!"
+para.style.color = 'red';
+whole.appendChild(para);
+
+const bluerino = document.createElement('h3');
+bluerino.style.color = 'blue';
+bluerino.textContent = "I'm a blue h3!";
+whole.appendChild(bluerino);
+
+const diverino = document
