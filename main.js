f355dc222c90b56d2a90b8efaab60059b3120c8f
Steven <stevenescobar131@gmail.com>
2022-03-02 19:55:31 -0600

    Working

--- main.js
+++ main.js
@@ -11,4 +11,15 @@ bluerino.style.color = 'blue';
 bluerino.textContent = "I'm a blue h3!";
 whole.appendChild(bluerino);
 
-const diverino = document
+const diverino = document.createElement('div');
+diverino.style.border = "black solid";
+diverino.style.backgroundColor = 'pink';
+whole.appendChild(diverino);
+
+const h_erino = document.createElement('h1');
+h_erino.textContent = "I'm in a div";
+diverino.appendChild(h_erino);
+
+const p_erino = document.createElement('p');
+p_erino.textContent = "ME TOO!";
+diverino.appendChild(p_erino);
